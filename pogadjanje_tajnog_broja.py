64a05b33192856d00293679c0abb491b3dbebc1b
Mirko Primorac <mirko.primorac@inet.hr>
2017-06-15 15:44:26 +0200

    1. U funkciji main je izbacena varijabla tajni_broj, a funkcija generiranje_tajnog_broja direktno ubacena u while petlju 2. Zamijenjen ispis sada nepostojece varijable tajni_broj sa varijablom pokušaj, koja je tada jednaka tajnom broju

--- pogadjanje_tajnog_broja.py
+++ pogadjanje_tajnog_broja.py
@@ -13,11 +13,26 @@ def main():
 	print('Program je igra pogađanja brojeva!\nGeneriran je tajni broj od 0-9, koji trebate pogoditi!')
 	pokušaj = int(input('Upišite neki broj od 0-9: ')) #Unos broja od strane korisnika.
 	broj_pokušaja = 1 #Postavljanje varijable broja pokušaja na jedan.
+	
+	'''dio starog koda - poziv funkcije "generiranje_tajnog_broja" je sada direktno iz petlje...
+	-------------------
 	tajni_broj = generiranje_tajnog_broja() #Pozivanje funkcije i upisivanje njezine izlazne vrijednosti u varijablu
 	while pokušaj != tajni_broj: #Petlja za provjeru rezultata.
+	-------------------
+	'''
+
+	while pokušaj != generiranje_tajnog_broja(): #Pozivanje funkcije i usporedba izlaza u petlji
 		pokušaj = int(input('Niste pogodili, pokušajte ponovo: ')) #Unos novog broja.
 		broj_pokušaja += 1 #Inkrementiranje broja pokušaja za jedan.
-	print('Pogodili ste broj u {}. pokušaju! Traženi broj je bio broj {}!'.format(broj_pokušaja, tajni_broj))
+	
+	'''dio starog koda - zamijenjena sada nepostojeca varijabla "tajni_broj" iz funkcije "generiranje_tajnog_broja"
+	------------------
+	#print('Pogodili ste broj u {}. pokušaju! Traženi broj je bio broj {}!'.format(broj_pokušaja, tajni_broj))
+	------------------
+	'''
+
+	print('Pogodili ste broj u {}. pokušaju! Traženi broj je bio broj {}!'.format(broj_pokušaja, pokušaj))
+	#Ispis i pozivanje varijable "pokusaj" koja je sada jednaka izlazu iz f. "generiranje_tajnog_broja"
 
 if __name__ == '__main__':
 	main()
